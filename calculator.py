7afe99e69be0744ac7b2ef85e6f3c259a9545678
Anurag Singh Yadav <117581504+AnuragSinghYadav@users.noreply.github.com>
2024-07-05 16:42:35 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,22 @@
+#making calculator using match case 
+num_1 = int(input("Enter the number1 = "))
+num_2 =  int(input("Enter the number2 ="))
+
+operator = (input("Enter operator: "))
+
+match operator:
+    case "+":
+        print("Sum is", num_1 + num_2)
+    case "-":
+        print("Difference is", num_1 - num_2)
+    case "*":
+        print("Product is", num_1 * num_2)
+    case "/":
+        print("Division is  = ", num_1 / num_2)
+    case"%":
+        print("Percentage is", num_1 % num_2)
+    case _ :
+        print("Enter a valid operatoer")            
+
+        
+        
